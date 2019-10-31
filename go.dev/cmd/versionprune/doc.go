689fe959ee0b7a89fddac38374c00a5b33824266
Alexander Rakoczy <rakoczy@google.com>
2019-10-31 19:44:27 -0400

    [x/go.dev] cmd/versionprune: add command to prune appengine versions
    This change adds a command to prune old app engine versions. AppEngine
    has a limit of 210 versions across all services. As we have many
    services in this project and deploy go-dev and learn-go-dev daily, we
    should clean up services regularly.
    
    There's no gcloud command to do something similar, and doing it in the
    UI is tedious and error prone.
    
    The command by default will:
    - keep the latest 5 versions
    - keep any version that is serving traffic
    - keep any version that is younger than 24h
    - ignore versions with invalid dates (doesn't seem to be a real thing)
    
    Updates b/143768957
    
    Change-Id: I315ca9f459aa28b8ae94b55557e628ceeb22c884
    Reviewed-on: https://team-review.git.corp.google.com/c/golang/go.dev/+/589755
    CI-Result: Cloud Build <devtools-proctor-result-processor@system.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    X-GoDev-Commit: b763768db4b9b9b2fcd67a3f5a0f3d18fe65cd9e

---
+++ go.dev/cmd/versionprune/doc.go
@@ -0,0 +1,42 @@
+/*
+Binary versionprune prunes stale AppEngine versions for a specified service.
+
+The command by default will:
+- keep the latest 5 versions
+- keep any version that is serving traffic
+- keep any version that is younger than 24h
+- ignore versions with invalid dates (doesn't seem to be a real thing)
+
+	Sample output:
+
+	target project:	[go-discovery]
+	target service:	[go-dev]
+	versions:	(18)
+
+	versions to keep (11): [
+		20191101t013408: version is serving traffic. split: 100%
+		20191031t211924: keeping the latest 5 versions (2)
+		20191031t211903: keeping the latest 5 versions (3)
+		20191031t205920: keeping the latest 5 versions (4)
+		20191031t232247: keeping the latest 5 versions (5)
+		20191031t232028: keeping recent versions (2h56m4.73591512s)
+		20191031t220312: keeping recent versions (4h13m20.735921508s)
+		20191031t211935: keeping recent versions (4h56m55.73592447s)
+		20191031t211824: keeping recent versions (4h58m10.735928067s)
+		20191031t200353: keeping recent versions (6h12m38.735932792s)
+		20191031t150644: keeping recent versions (11h9m44.735935312s)
+	]
+	versions to delete (7): [
+		20191030t225128: bye
+		20191030t214823: bye
+		20191030t214355: bye
+		20191030t204338: bye
+		20191030t202841: bye
+		20191030t195403: bye
+		20191030t192250: bye
+	]
+	deleting go-discovery/go-dev/20191030t225128
+	...
+
+*/
+package main
--- go.dev/go.mod
+++ go.dev/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/google/go-cmp v0.3.1
 	github.com/microcosm-cc/bluemonday v1.0.2
+	google.golang.org/api v0.13.0
 	gopkg.in/yaml.v2 v2.2.2
 )
