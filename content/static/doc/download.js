fb84a6e988889adb417e48cd23f99b39891353d3
Steve Traut <straut@google.com>
2020-08-03 14:34:38 -0400

    content/static/doc: improve getting started docs with install topic and tutorials
    The existing install topic includes content that is likely not needed by
    most new users, including system requirements, content on installing
    multiple versions, and so on. This change separates the most common install
    case content into its own topic and moves some of the less commonly used
    content into a separate topic.
    
    This change also includes an introductory tutorial that might be useful for
    new users, particularly those coming from a different language. A second
    multi-part tutorial continues the introduction with fundamental programming
    topics connected by a narrative thread and featuring links to other related
    parts of the docs.
    
    This change includes:
    
    - A simpler "Download and install" topic that replaces Getting started.
    Topic is designed to help users focus on what they need for a given OS.
    - A new "Managing Go installations" topic that holds content about
    installing that is likely not in the most common workflow (including
    uninstall, multiple versions).
    - A minor change that adds a link to the install-source topic.
    - A single-page Hello World tutorial.
    - A multi-part tutorial that introduces fundamental programming concepts
    from a Go perspective (functions, loops, errors, slices, maps, and so on).
    - An index file that lists the tutorials.
    - CSS to style "Note" sections, tabbed sections, table, and highlighted
    code.
    - A JavaScript file with code for the install page.
    
    This change also creates on golang/website a version of install.html and
    install-source.html, which would be deleted from the golang/go repository
    (effectively moving the files).
    
    For golang/go#40496
    
    Change-Id: I2af6f5fbbcfcf887c8698919666155cc40ce0d03
    Reviewed-on: https://go-review.googlesource.com/c/website/+/246499
    Reviewed-by: Russ Cox <rsc@golang.org>
    Reviewed-by: Andrew Bonventre <andybons@golang.org>

---
+++ content/static/doc/download.js
@@ -0,0 +1,125 @@
+class DownloadsController {
+  constructor() {
+    // Parts of tabbed section.
+    this.tablist = document.querySelector('.js-tabSection');
+    this.tabs = this.tablist.querySelectorAll('[role="tab"]');
+    this.panels = document.querySelectorAll('[role="tabpanel"]');
+
+    // OS for which to display download and install steps.
+    this.osName = 'Unknown OS';
+
+    // URL to JSON containing list of installer downloads.
+    const fileListUrl = 'https://golang.org/dl/?mode=json';
+    this.activeTabIndex = 0;
+
+    // Get the install file list, then get names and sizes
+    // for each OS supported on the install page.
+    fetch(fileListUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const files = data[0]['files'];
+        for (var i = 0; i < files.length; i++) {
+          let file = files[i].filename;
+          let fileSize = files[i].size;
+          if (file.match('.linux-amd64.tar.gz$')) {
+            this.linuxFileName = file;
+            this.linuxFileSize = Math.round(fileSize / Math.pow(1024, 2));
+          }
+          if (file.match('.darwin-amd64(-osx10.8)?.pkg$')) {
+            this.macFileName = file;
+            this.macFileSize = Math.round(fileSize / Math.pow(1024, 2));
+          }
+          if (file.match('.windows-amd64.msi$')) {
+            this.windowsFileName = file;
+            this.windowsFileSize = Math.round(fileSize / Math.pow(1024, 2));
+          }
+        }
+        this.detectOS();
+        document.getElementById(this.osName).click();
+        this.setDownloadForOS(this.osName);
+      })
+      .catch(console.error);
+      this.setEventListeners();
+  }
+
+  setEventListeners() {
+    this.tabs.forEach((tabEl) => {
+      tabEl.addEventListener('click', e => this.handleTabClick((e)));
+    });
+  }
+
+  // Set the download button UI for a specific OS.
+  setDownloadForOS(osName) {
+    const baseURL = 'https://golang.org/dl/';
+    let download;
+
+    switch(osName){
+      case 'linux':
+        document.querySelector('.js-downloadButton').textContent =
+          'Download Go for Linux';
+        document.querySelector('.js-downloadDescription').textContent =
+          this.linuxFileName + ' (' + this.linuxFileSize + ' MB)';
+        document.querySelector('.js-download').href = baseURL + this.linuxFileName;
+        break;
+      case 'mac':
+        document.querySelector('.js-downloadButton').textContent =
+          'Download Go for Mac';
+        document.querySelector('.js-downloadDescription').textContent =
+          this.macFileName + ' (' + this.macFileSize + ' MB)';
+        document.querySelector('.js-download').href = baseURL + this.macFileName;
+        break;
+      case 'windows':
+        document.querySelector('.js-downloadButton').textContent =
+          'Download Go for Windows';
+        document.querySelector('.js-downloadDescription').textContent =
+          this.windowsFileName + ' (' + this.windowsFileSize + ' MB)';
+        document.querySelector('.js-download').href = baseURL + this.windowsFileName;
+        break;
+      default:
+        document.querySelector('.js-downloadButton').textContent = 'Download Go';
+        document.querySelector('.js-downloadDescription').textContent =
+          'Visit the downloads page.';
+        document.querySelector('.js-download').href = baseURL;
+        break;
+    }
+  }
+
+  // Detect the users OS for installation default.
+  detectOS() {
+    if (navigator.appVersion.indexOf('Linux') !== -1) {
+      this.osName = 'linux';
+    }
+    if (navigator.appVersion.indexOf('Mac') !== -1) {
+      this.osName = 'mac';
+    }
+    if (navigator.appVersion.indexOf('X11') !== -1) {
+      this.osName = 'unix';
+    }
+    if (navigator.appVersion.indexOf('Win') !== -1) {
+      this.osName = 'windows';
+    }
+  }
+
+  // Activates the tab at the given index.
+  activateTab(index) {
+    this.tabs[this.activeTabIndex].setAttribute('aria-selected', 'false');
+    this.tabs[this.activeTabIndex].setAttribute('tabindex', '-1');
+    this.panels[this.activeTabIndex].setAttribute('hidden', '');
+    this.tabs[index].setAttribute('aria-selected', 'true');
+    this.tabs[index].setAttribute('tabindex', '0');
+    this.panels[index].removeAttribute('hidden');
+    this.tabs[index].focus();
+    this.activeTabIndex = index;
+  }
+
+  // Handles clicks on tabs.
+  handleTabClick(e) {
+    const el = (e.target);
+    this.activateTab(Array.prototype.indexOf.call(this.tabs, el));
+    this.setDownloadForOS(el.id);
+  }
+
+}
+
+// Instantiate controller for page event handling.
+new DownloadsController();
