c818595f3d30d930fd77890df7ab2e240ea9525e
Roland Shoemaker <rolandshoemaker@gmail.com>
2021-01-13 10:47:31 -0800

    internal/history: document upcoming Go 1.15.7 and 1.14.14 security releases
    They have been pre-announced¹.
    
    ¹ https://groups.google.com/g/golang-announce/c/KvrRblbXp_w
    
    Change-Id: I6c55559a8598b8417f92bc1e8c69fd974270f462
    Reviewed-on: https://go-review.googlesource.com/c/website/+/283536
    Run-TryBot: Roland Shoemaker <roland@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Roland Shoemaker <roland@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

--- internal/history/release.go
+++ internal/history/release.go
@@ -31,6 +31,19 @@ type Release struct {
 // It contains entries for releases of Go 1.9 and newer.
 // Older releases are listed in doc/devel/release.html.
 var Releases = map[GoVer]Release{
+	{1, 15, 7}: {
+		Date:     Date{2021, 1, 19},
+		Security: true,
+
+		Future: true,
+	},
+	{1, 14, 14}: {
+		Date:     Date{2021, 1, 19},
+		Security: true,
+
+		Future: true,
+	},
+
 	{1, 15, 6}: {
 		Date: Date{2020, 12, 3},
 
