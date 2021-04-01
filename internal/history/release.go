363b0256a35b31dbbe340e0cefa42805cb43ff85
Dmitri Shuralyov <dmitshur@golang.org>
2021-04-01 15:17:42 -0400

    internal/history: document Go 1.16.3 and Go 1.15.11
    For golang/go#45334.
    For golang/go#45333.
    
    Change-Id: I7928cf48bad658ca7dd391b84802e0c8bb788617
    Reviewed-on: https://go-review.googlesource.com/c/website/+/306670
    Trust: Dmitri Shuralyov <dmitshur@golang.org>
    Run-TryBot: Dmitri Shuralyov <dmitshur@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>

--- internal/history/release.go
+++ internal/history/release.go
@@ -31,6 +31,19 @@ type Release struct {
 // It contains entries for releases of Go 1.9 and newer.
 // Older releases are listed in doc/devel/release.html.
 var Releases = map[GoVer]Release{
+	{1, 16, 3}: {
+		Date: Date{2021, 4, 1},
+
+		Components: []template.HTML{"the compiler", "linker", "runtime", "the <code>go</code> command"},
+		Packages:   []string{"testing", "time"},
+	},
+	{1, 15, 11}: {
+		Date: Date{2021, 4, 1},
+
+		Components: []template.HTML{"cgo", "the compiler", "linker", "runtime", "the <code>go</code> command"},
+		Packages:   []string{"database/sql", "net/http"},
+	},
+
 	{1, 16, 2}: {
 		Date: Date{2021, 3, 11},
 
