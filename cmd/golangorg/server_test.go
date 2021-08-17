dda655f33052966bca89230f36a66713e4b91913
Russ Cox <rsc@golang.org>
2021-08-17 12:56:39 -0400

    cmd/golangorg: fix ref/mod to be template-safe and add exhaustive template syntax test
    Fixes golang/go#47751.
    
    Change-Id: I28a979eee3c75749449291d0b60bc64a6d761ae0
    Reviewed-on: https://go-review.googlesource.com/c/website/+/342899
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>

--- cmd/golangorg/server_test.go
+++ cmd/golangorg/server_test.go
@@ -5,8 +5,12 @@
 package main
 
 import (
+	"bytes"
+	"net/http/httptest"
+	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
 	"golang.org/x/website/internal/webtest"
@@ -28,4 +32,33 @@ func TestWeb(t *testing.T) {
 			webtest.TestHandler(t, file, h)
 		}
 	}
+}
+
+func TestAll(t *testing.T) {
+	h := NewHandler("../../_content", runtime.GOROOT())
+
+	testTree := func(dir, prefix string) {
+		filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				t.Fatal(err)
+			}
+			path = filepath.ToSlash(path)
+			if strings.HasSuffix(path, ".md") {
+				rec := httptest.NewRecorder()
+				rec.Body = new(bytes.Buffer)
+				url := prefix + strings.TrimSuffix(strings.TrimPrefix(path, dir), ".md")
+				if strings.HasSuffix(url, "/index") {
+					url = strings.TrimSuffix(url, "index")
+				}
+				h.ServeHTTP(rec, httptest.NewRequest("GET", url, nil))
+				if rec.Code != 200 && rec.Code != 301 {
+					t.Errorf("GET %s: %d, want 200\n%s", url, rec.Code, rec.Body.String())
+				}
+			}
+			return nil
+		})
+	}
+
+	testTree("../../_content", "https://golang.org")
+	testTree("../../go.dev/_content", "https://go.dev")
 }
