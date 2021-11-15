163df21ffa3f74771a46bd0a642cde4b35e0ed5f
Bartłomiej Klimczak <bartlomiej.klimczak88@gmail.com>
2021-11-15 18:58:07 +0100

    replace `+build ignore` with `go:build ignore`

--- _content/doc/articles/wiki/final-template.go
+++ _content/doc/articles/wiki/final-template.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build ignore
+//go:build ignore
 
 package main
 
 import (
 	"html/template"
-	"os"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Page struct {
