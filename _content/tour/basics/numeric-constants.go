245dc6efa450e71436e3cb041c9b182a6a4ab369
Abhishek01039 <abhishekghaskata1999@gmail.com>
2022-01-03 22:33:47 +0530

    formating the code

--- _content/tour/basics/numeric-constants.go
+++ _content/tour/basics/numeric-constants.go
@@ -12,7 +12,7 @@ const (
 	Small = Big >> 99
 )
 
-func needInt(x int) int { return x*10 + 1 }
+func needInt(x int) int { return x * 10 + 1 }
 func needFloat(x float64) float64 {
 	return x * 0.1
 }
